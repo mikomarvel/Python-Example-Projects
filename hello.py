2384f6fec0aeeb048832234e2ea1cc3269dda835
mikomarvel <mikomarvel178@gmail.com>
2024-08-01 18:42:55 +0300

    Update hello.py
    Python Example Project_1

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
-print('hello  ethiopia')
-print('wauupp')
+#python example project_1
 
+from datetime import datetime #keeps the date updated!
+
+year = input("enter your year of birth.(Eg.1990)")
+month = input("enter your month of birth.(E.g 8 june)")
+day = input("enter your date of birth.")
+
+print("Y",year, "M", month, "D", day)
+
+x = 2024 - int(year)
+print("you are", x, "years old.")
