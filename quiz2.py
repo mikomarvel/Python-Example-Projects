e5bf72858b441f9cf4fcb00a0ebbf8b0f6d2c85d
mikomarvel <mikomarvel178@gmail.com>
2024-08-09 10:37:40 +0300

    change

--- quiz2.py
+++ quiz2.py
@@ -2,17 +2,22 @@
 #NUMBER_GUESSING_GAME
 
 import random #imports a random number for a fair play
-correct_guess = random.randint(1, 10) #generates a random integer
 
-user_guess = int(input("enter your guess from 1-10:- ")) #casting the input data to get it as a number.
-if user_guess == correct_guess: #using this(==) because this(=)is used for assigning.
-    print(f"you guessed right🫡{correct_guess}")
+while True:
+     correct_guess = random.randint(1, 10) #generates a random integer
+     attempt = 0
+     while attempt < 3:
+                attempt = attempt + 1
+                print(f"This is your {attempt} attempt.")
+                user_guess = int(input("enter your guess from 1-10:- ")) #casting the input data to get it as a number.
+                if user_guess == correct_guess: #using this(==) because this(=)is used for assigning.
+                    print(f"you guessed right🫡---> {correct_guess}")
+                    break 
+                elif user_guess > correct_guess:
+                    print(f"a bit higher than the correct answer!! THE CORRECT GUESS IS-->{correct_guess} ")
 
-elif user_guess > correct_guess:
-    print(f"a bit higher than the correct answer!! {correct_guess}")
+                elif user_guess < correct_guess:
+                    print(f"you are bit lower than the correct guess.!! THE CORRECT GUESS IS-->{correct_guess} ")
 
-elif user_guess < correct_guess:
-    print(f"you are bit lower than the correct guess.!!{correct_guess}")
-
-else:
-    print("wroooong🫠")
+                else:
+                    print("wroooong🫠")
